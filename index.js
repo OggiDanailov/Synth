9f3414e96f7c0ffc8a002e2d703d62d34400f859
Ognian <ogidan@abv.bg>
2015-08-17 15:22:04 -0400

    key scale on HTMl and css created

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
   context = new AudioContext();
-  var osc = document.getElementById("osc");
+  var osc = document.getElementById("C");
+  var waves_sine = document.getElementById("waves_sine");
+  var waves_triangle = document.getElementById("waves_triangle");
+  var waves_sawtooth = document.getElementById("waves_sawtooth");
+  var waves_square = document.getElementById("waves_square");
+  
  
 
 
   osc.onmousedown  = function() {
     var oscPitch = document.getElementById('oscPitch').value;
     oscillator = context.createOscillator();
-    oscillator.type = 'square';
+    oscillator.type = 'sawtooth';
     oscillator.frequency.value = oscPitch;
     oscillator.connect(context.destination);
     oscillator.start();
   };
+
+  
+
+
 
   osc.onmouseup = function() {
     oscillator.disconnect();
