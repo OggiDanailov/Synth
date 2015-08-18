197602cd14bcf1dba13ef8cc85d09494c66aca56
Ognian <ogidan@abv.bg>
2015-08-18 17:19:08 -0400

    got sort of sound

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-
-
   context = new AudioContext();
   var osc = document.getElementById("C");
 
-  var waves_sine = document.getElementById("waves_sine");
-  var waves_triangle = document.getElementById("waves_triangle");
-  var waves_sawtooth = document.getElementById("waves_sawtooth");
-  var waves_square = document.getElementById("waves_square");
   
- 
-
 
   osc.onmousedown  = function() {
     var oscPitch = document.getElementById('oscPitch').value;
     oscillator = context.createOscillator();
     oscillator.type = 'triangle';
-    oscillator.frequency.value = 120;
+    oscillator.frequency.value = 220;
     oscillator.connect(context.destination);
     oscillator.start();
   };
--- notes.js
+++ notes.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 context = new AudioContext();
 
-var oscillators = document.getElementsByClassName("keys");
-  _.each(oscillators, function(osc, i){
-  keyNum = i + 16;
+
+// Assign corresponding frequency to each key
+// _.range(40, 52).map / function(n){
+// exp = (n - 49)/12
+// return Math.pow(2, exp) * 440; }
+
+
+var oscillators = document.getElementsByClassName("key");
+_.each(oscillators, function(osc, i){
+  keyNum = i + 40;
   exp = (keyNum - 49)/12
   var freq = Math.pow(2, exp) * 440;
-osc.onmousedown  = function() {
+  osc.onmousedown  = function() {
     oscillator = context.createOscillator();
     oscillator.type = 'sine';
     oscillator.frequency.value = freq;
@@ -14,28 +21,20 @@ osc.onmousedown  = function() {
     oscillator.start();
   };
   
-osc.onmouseup = function() {
+  osc.onmouseup = function() {
     oscillator.disconnect();
-  };
+  }; 
 
 });
 
+  
+
+  
+
+  
+
+  
 
 
 
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
