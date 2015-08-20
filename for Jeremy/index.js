f299f08311408643a836a2e1092edb8bfb235893
Ognian <ogidan@abv.bg>
2015-08-20 14:59:07 -0400

    special folder for jeremy created

---
+++ for Jeremy/index.js
@@ -0,0 +1,92 @@
+var pianoKeys =  [{name: "C1", color: "white"}, {name: "Db1", color: "black"},
+                  {name: "D1", color: "white"}, {name: "Eb1", color: "black"},
+                  {name: "E1", color: "white"}, 
+                  {name: "F1", color: "white"}, {name: "Gb1", color: "black"},
+                  {name: "G1", color: "white"}, {name: "Ab1", color: "black"},
+                  {name: "A1", color: "white"}, {name: "Bb1", color: "black"},
+                  {name: "B1", color: "white"},
+
+                  {name: "C2", color: "white"}, {name: "Db2", color: "black"},
+                  {name: "D2", color: "white"}, {name: "Eb2", color: "black"},
+                  {name: "E2", color: "white"}, 
+                  {name: "F2", color: "white"}, {name: "Gb2", color: "black"},
+                  {name: "G2", color: "white"}, {name: "Ab2", color: "black"},
+                  {name: "A2", color: "white"}, {name: "Bb2", color: "black"},
+                  {name: "B2", color: "white"},
+
+                  {name: "C3", color: "white"}, {name: "Db3", color: "black"},
+                  {name: "D3", color: "white"}, {name: "Eb3", color: "black"},
+                  {name: "E3", color: "white"}, 
+                  {name: "F3", color: "white"}, {name: "Gb3", color: "black"},
+                  {name: "G3", color: "white"}, {name: "Ab3", color: "black"},
+                  {name: "A3", color: "white"}, {name: "Bb3", color: "black"},
+                  {name: "B3", color: "white"},
+
+                  {name: "C4", color: "white"}, {name: "Db4", color: "black"},
+                  {name: "D4", color: "white"}, {name: "Eb4", color: "black"},
+                  {name: "E4", color: "white"}, 
+                  {name: "F4", color: "white"}, {name: "Gb4", color: "black"},
+                  {name: "G4", color: "white"}, {name: "Ab4", color: "black"},
+                  {name: "A4", color: "white"}, {name: "Bb4", color: "black"},
+                  {name: "B4", color: "white"},
+                  
+                  {name: "C5", color: "white"}, {name: "Db5", color: "black"},
+                  {name: "D5", color: "white"}, {name: "Eb5", color: "black"},
+                  {name: "E5", color: "white"}, 
+                  {name: "F5", color: "white"}, {name: "Gb5", color: "black"},
+                  {name: "G5", color: "white"}, {name: "Ab5", color: "black"},
+                  {name: "A5", color: "white"}, {name: "Bb5", color: "black"},
+                  {name: "B5", color: "white"},
+                  ] 
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  context = new AudioContext();
+  $whiteContainer = $('#white');
+  $blackContainer = $('#black');
+
+    var pianoKeysFreq = _.map(pianoKeys, function(pianoKeys, i){
+      keyNum = i + 16;
+      exp = (keyNum - 49) / 12
+      var frequency = Math.pow(2, exp) * 440;
+      return _.extend(pianoKeys, {frequency:frequency})
+    })
+
+    whites = _.where(pianoKeys, {color: "white"});
+    blacks = _.where(pianoKeys, {color: "black"});
+
+    function populateKeys(keys){
+
+      _.each(keys, function(key) {
+        name = key.name;
+        id = key.name;
+        color = key.color;
+        frequency = key.frequency;
+        
+        div = "<div class='" + color + "' id='" + id + "'></div>";
+        
+        if(color == "white"){
+          $(div).appendTo($whiteContainer)
+        } else {
+          $(div).appendTo($blackContainer)
+        };
+
+        oscillator = null
+        
+        $("#" + id).on('mousedown', function(){  
+          oscillator = context.createOscillator();        
+          oscillator.type = 'sine';
+          oscillator.frequency.value = frequency;
+          oscillator.connect(context.destination);
+          oscillator.start();
+        });
+
+        $("#" + id).on('mouseup', function() {
+          oscillator.disconnect();
+        });
+      })  
+    }
+    populateKeys(whites);
+    populateKeys(blacks);
+
+});
+
